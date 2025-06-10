17c005e84186159bb8125d96e685096b2b372140
Ei-Dolon <138881746+Ei-Dolon@users.noreply.github.com>
2025-06-10 06:23:15 +0100

    Add files via upload

---
+++ src/components/ConnectWallet.tsx
@@ -0,0 +1,9 @@
+//src/components/ConnectWallet.tsx
+import React from 'react';
+import { AppKitButton } from '@reown/appkit';
+
+const ConnectWallet: React.FC = () => {
+  return <AppKitButton />;
+};
+
+export default ConnectWallet;
