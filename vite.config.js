d1913e2908d15d87b3f2b1cf5a806265fe4ec663
Ei-Dolon <138881746+Ei-Dolon@users.noreply.github.com>
2025-06-11 07:15:29 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from "node:path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    loader: { '.js': 'jsx' }
+  },
+  outDir: "build",
   plugins: [react()],
+  server: {
+    origin: 'https://jetsetlux.com/dapp',
+  },
   resolve: {
     alias: {
-      '@reown': path.resolve(__dirname, './src/reown')
-    }
+      '@': resolve(__dirname, './src'),
+    },
   }
-})
+});
+
