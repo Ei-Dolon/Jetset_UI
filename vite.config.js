ad72b610f9401a96d25db8538720d389056b7fd2
Ei-Dolon <138881746+Ei-Dolon@users.noreply.github.com>
2025-06-11 06:26:56 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+//import tailwindcss from "@tailwindcss/vite";
 
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
