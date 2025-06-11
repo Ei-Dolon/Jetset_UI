98c9e3e739d92ffedaeb338fe37a96e801cbd9c7
Ei-Dolon <138881746+Ei-Dolon@users.noreply.github.com>
2025-06-11 06:30:49 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 //import tailwindcss from "@tailwindcss/vite";
 
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { resolve } from "node:path";
+//import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+//import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  }
+  plugins: [react()],
+
 });
