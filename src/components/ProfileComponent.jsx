caf067c286df087cc359acdc2c1370eecedcbbf7
Ei-Dolon <138881746+Ei-Dolon@users.noreply.github.com>
2025-06-14 09:54:50 +0100

    Delete src/components/ProfileComponent.jsx

--- src/components/ProfileComponent.jsx
+++
@@ -1,8 +0,0 @@
-// ProfileComponent
-
-// default currency to use
-localStorage.setItem("currency","USD");
-
-// ProfileComponent Popup - config form - local currency selection radio buttons
-document.getElementById("currencyRadioBtn").innerHTML = localStorage.getItem("currency");
-
